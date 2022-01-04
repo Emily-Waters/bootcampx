3ca7ba6e6e60693a6616b219a4251ab2251d802b
Emily-Waters <emilyabigailwaters@gmail.com>
2022-01-04 11:55:34 +0000

    Added 11_total_assignments_and_duration.sql

---
+++ 4_queries/11_total_assignments_and_duration.sql
@@ -0,0 +1,12 @@
+SELECT
+  assignments.day,
+  COUNT(assignments.*)
+    AS number_of_assignments,
+  SUM(assignments.duration)
+    AS duration
+FROM
+  assignments
+GROUP BY
+  assignments.day
+ORDER BY
+  assignments.day ASC;
