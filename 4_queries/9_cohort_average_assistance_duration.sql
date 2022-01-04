627bad3397da7fd4ab62500b16a5fef5c848be7d
Emily-Waters <emilyabigailwaters@gmail.com>
2022-01-04 11:39:52 +0000

    Added 9_cohort_average_assistance_duration.sql

---
+++ 4_queries/9_cohort_average_assistance_duration.sql
@@ -0,0 +1,21 @@
+SELECT
+  AVG(total_duration)
+    AS average_total_duration
+FROM (
+  SELECT
+    cohorts.name,
+    SUM(assistance_requests.completed_at - assistance_requests.started_at)
+      AS total_duration
+  FROM
+    assistance_requests
+  JOIN
+    students
+      ON assistance_requests.student_id = students.id
+  JOIN
+    cohorts
+      ON students.cohort_id = cohorts.id
+  GROUP BY 
+    cohorts.name
+  ORDER BY total_duration ASC
+) AS total_duration
+;
